37cad2e9738ed38f500134f8ee0a41c453c088a0
jackfred <1120429511@qq.com>
2017-11-03 23:23:46 -0700

    hash

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import store from './store';
 import App from './App.jsx';
 import Test from './pages/test/index.jsx';
 import './index.css';
 const routes = (
     <Provider store={store}>
-        <Router history={browserHistory}>
+        <Router history={hashHistory}>
             <Route path="/" component={App}>
                 <IndexRoute component={Test} />
                 <Route path="test" component={Test}/>
